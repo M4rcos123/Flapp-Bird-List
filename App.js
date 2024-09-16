b79e11be2926dd213fac4eccbd228d38f2888fda
M4rcos123 <marcos.junior@aluno.faeterj-brm.faetec.rj.gov.br>
2024-09-16 12:00:43 -0300

    primeiro

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { View, Text, Button, FlatList, TextInput, StyleSheet } from 'react-native';
+import { api } from './src/services/api'; 
+import { styles } from './src/styles/styles'; 
+
+function App() {
+  const [dados, setDados] = useState([]);
+  const [inputValue, setInputValue] = useState(''); 
+
+  async function searchDados() {
+    const response = await api.get(`${inputValue}` + '/repos');
+    console.log(response);
+    setDados(response.data);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button title="Consultar" onPress={searchDados} />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={inputValue}
+          onChangeText={setInputValue}
+          placeholder="Quem quer achar?"
+          placeholderTextColor= "#0f0d0f"
+        />
+      </View>
+
+      <FlatList
+        style={styles.list}
+        data={dados}
+        keyExtractor={(item) => item.id.toString()} 
+        renderItem={({ item }) => (
+          <View style={styles.itemList}>
+            <Text style={styles.itemText}>ID: {item.id}</Text>
+            <Text style={styles.itemText}>Node ID: {item.node_id}</Text>
+            <Text style={styles.itemText}>Name: {item.name}</Text>
+            <Text style={styles.itemText}>Full Name: {item.full_name}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+export default App;
+
+
---
+++ src/styles/styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from  'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#a35fde',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonContainer: {
+      marginTop: 50,
+      backgroundColor: '#f078e2',
+      color: '#0f0d0f',
+      marginBottom: 20,
+      width: '80%',
+    },
+    list: {
+      marginTop: 32,
+      width: '80%',
+      borderRadius: 7,
+    },
+    itemList: {
+      padding: 24,
+      borderRadius: 7,
+      backgroundColor: '#f078e2',
+      marginBottom: 16,
+    },
+    itemText: {
+      color: '#0f0d0f',
+    },
+    inputContainer: {
+      marginTop: 20,
+      width: '80%', 
+    },
+    input:{
+      height: 40, 
+      borderColor: '#0f0d0f',
+      borderWidth: 1,
+      borderRadius: 5,
+      paddingHorizontal: 10,
+      paddingVertical: 8, 
+      color: '#0f0d0f', 
+    }
+  });
